410ca1e943b225e08068b23b8be82ff4db6518a5
Jacob Duell <jrduell7@gmail.com>
2019-03-27 19:43:24 -0500

    lesson-06 refactoring in xcode/linking view controllers with classes

---
+++ swoosh-app/swoosh-app/Controller/LeagueVC.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueVC.swift
+//  swoosh-app
+//
+//  Created by jacob on 3/27/19.
+//  Copyright © 2019 jacob. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
